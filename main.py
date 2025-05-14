21abfc051a25aaccec851699a9ff9f6be9f08cad
Bohdan Tron <bohdan.tron@gmail.com>
2025-05-14 11:00:03 +0300

    feat: subject added, email limited and formatted

--- main.py
+++ main.py
@@ -8,24 +8,28 @@ load_dotenv(dotenv_path="envs/.env")
 
 api_key = os.environ.get('NEWS_API_KEY')
 debug_mode = os.environ.get('DEBUG')
-url = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={api_key}"
+url = f"https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey={api_key}&language=en"
 
 
 r = requests.get(url)
 data = r.json()
 
-news_mail = []
+news_mail = ["Subject: Health News Today "]
 
-for article in data['articles']:
+for article in data['articles'][:5]:
    # Parse the published date
   published_date = datetime.strptime(article['publishedAt'], "%Y-%m-%dT%H:%M:%SZ")
   # Format date as DD Mon YY
   formatted_date = published_date.strftime("%d %b %y")
   
-  formatted_post = f'{article["url"]} {article["title"]}, {formatted_date}' + '\n' + f'{article["description"]}' + 2 * '\n'
+  formatted_post = formatted_date + '\n' \
+    + f'{article["title"]}' + '\n' \
+    + article["url"] + '\n' \
+    + f'{article["description"]}' + 2 * '\n'
+  
   news_mail.append(formatted_post)
   
 news_mail = ''.join(news_mail)
 news_mail = news_mail.encode('utf-8')
-print('mail sent')
+
 send_email(news_mail)
--- send_email.py
+++ send_email.py
@@ -13,16 +13,3 @@ def send_email(message):
   with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message)
-# def send_email(message):
-#     host = "smtp.gmail.com"
-#     port = 465
-
-#     username = "app8flask@gmail.com"
-#     password = "pmuuyjaxfejfpxkc"
-
-#     receiver = "app8flask@gmail.com"
-#     context = ssl.create_default_context()
-
-#     with smtplib.SMTP_SSL(host, port, context=context) as server:
-#         server.login(username, password)
-#         server.sendmail(username, receiver, message)
